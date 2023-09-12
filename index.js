ba2136d500634260ac14b18603b9ac891b421330
KINGBOTs1 <ds767365@gmail.com>
2023-09-12 09:16:54 +0800

    new

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import {
     fileURLToPath
-} from 'url'; // Import fileURLToPath
-import * as path from 'path'; // Import path
+} from 'url';
+import * as path from 'path';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 import multer from 'multer';
@@ -20,25 +20,25 @@ const port = process.env.PORT || 3000;
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 });
-app.use(cors());
-app.use(express.json());
-
-// Menggunakan import.meta.url dan fileURLToPath untuk mengakses __dirname
-const __filename = fileURLToPath(
-    import.meta.url);
-const __dirname = path.dirname(__filename);
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html')); // Menggunakan path.join untuk menggabungkan __dirname dengan path ke index.html
 });
 
-// Konfigurasi multer untuk menangani file audio
-const storage = multer.memoryStorage(); // Simpan file dalam memori
+app.use(cors());
+app.use(express.json());
+
+const __filename = fileURLToPath(
+    import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Menggunakan multer untuk menangani file audio
+const storage = multer.memoryStorage();
 const upload = multer({
     storage
 });
 
-app.use(express.static(path.join(__dirname, '/')));
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/audio', upload.single('audio'), async (req, res) => {
     if (!req.file) {
@@ -50,67 +50,79 @@ app.post('/audio', upload.single('audio'), async (req, res) => {
     // Simpan file audio yang diunggah
     const audioBuffer = req.file.buffer;
     const audioFileName = `voice_message_${Date.now()}.wav`;
-    const audioFilePath = path.join(__dirname, '/', audioFileName);
+    const audioFilePath = path.join(__dirname, 'public', 'audio', audioFileName);
 
     fs.writeFileSync(audioFilePath, audioBuffer);
 
     // Konversi file audio ke format MP3
     const mp3FileName = audioFileName.replace('.wav', '.mp3');
-    const mp3FilePath = path.join(__dirname, '/', mp3FileName);
+    const mp3FilePath = path.join(__dirname, 'public', 'audio', mp3FileName);
 
     ffmpeg()
         .input(audioFilePath)
         .toFormat('mp3')
         .on('end', async () => {
+            // Hapus file WAV setelah konversi selesai
             fs.unlinkSync(audioFilePath);
-            const transcription = await openai.audio.transcriptions.create({
-                file: fs.createReadStream(mp3FilePath),
-                model: 'whisper-1',
-            });
 
-            console.log('Transkripsi Teks:');
-            console.log(transcription.text);
-            const transcriptionText = transcription.text;
-            const userMessage = transcriptionText;
-            const messages = [{
-                "role": "user",
-                "content": userMessage
-            }];
-            chatHistory.push(messages);
-            const completion = await openai.chat.completions.create({
-                model: "gpt-3.5-turbo",
-                messages: chatHistory.flat(),
-            });
+            // Setelah konversi, lakukan transkripsi teks
+            try {
+                const transcription = await openai.audio.transcriptions.create({
+                    file: fs.createReadStream(mp3FilePath),
+                    model: 'whisper-1',
+                });
 
-            let botResponse = completion.choices[0].message.content;
-            console.log(completion.choices[0].message.content);
+                console.log('Transkripsi Teks:');
+                console.log(transcription.text);
+                const transcriptionText = transcription.text;
 
-            // Menghapus spasi di awal dan akhir dari botResponse
-            botResponse = botResponse.trim();
-            chatHistory.push([{
-                "role": "assistant",
-                "content": botResponse
-            }]);
+                // Tanggapan Bot
+                const userMessage = transcriptionText;
+                const messages = [{
+                    "role": "user",
+                    "content": userMessage
+                }];
+                chatHistory.push(messages);
+                const completion = await openai.chat.completions.create({
+                    model: "gpt-3.5-turbo",
+                    messages: chatHistory.flat(),
+                });
 
-            console.log(chatHistory);
-            fs.unlinkSync(mp3FilePath);
-            res.json({
-                response: botResponse
-            });
+                let botResponse = completion.choices[0].message.content;
 
+                // Menghapus spasi di awal dan akhir dari botResponse
+                botResponse = botResponse.trim();
+                chatHistory.push([{
+                    "role": "assistant",
+                    "content": botResponse
+                }]);
 
+                console.log(chatHistory);
+
+                // Hapus file MP3 setelah selesai
+                fs.unlinkSync(mp3FilePath);
+
+                res.json({
+                    response: botResponse
+                });
+            } catch (error) {
+                console.error('Error:', error);
+                res.status(500).json({
+                    error: 'An error occurred during transcription'
+                });
+            }
         })
         .on('error', (err) => {
             console.error('Error:', err);
             res.status(500).json({
-                error: 'Terjadi kesalahan saat konversi audio'
+                error: 'An error occurred during audio conversion'
             });
         })
         .save(mp3FilePath);
 });
 
-
 const chatHistory = [];
+
 app.post('/chat', async (req, res) => {
     const userMessage = req.body.message;
     try {
@@ -125,7 +137,6 @@ app.post('/chat', async (req, res) => {
         });
 
         let botResponse = completion.choices[0].message.content;
-        console.log(completion.choices[0].message.content);
 
         // Menghapus spasi di awal dan akhir dari botResponse
         botResponse = botResponse.trim();
@@ -146,13 +157,13 @@ app.post('/chat', async (req, res) => {
         });
     }
 });
+
 app.post('/clear', (req, res) => {
-    chatHistory.length = 0; // Mengosongkan array chatHistory
+    chatHistory.length = 0;
     res.status(200).json({
         message: 'Chat history cleared'
     });
 });
-
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
